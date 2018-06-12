fad2a4ed6d46c0d6292eb4bd34505cf9773d5bba
Yashwanth <yashwanth.nr@gmail.com>
2018-06-12 14:27:18 +0530

    added api key to users and generate it

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :urls
+  before_create :generate_api_key
+
+  def generate_api_key
+    self.api_key = SecureRandom.hex
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   get 'urls/url_not_found'
   resources :urls
   devise_for :users
-  root to: "home#index"
+  root to: "urls#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
